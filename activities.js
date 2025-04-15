867fd362d49e1a73766160998ff3ed47e16fac0b
RayJay1108 <rjvargas0804@gmail.com>
2025-04-15 14:32:51 +0800

    trouble shooting

---
+++ activities.js
@@ -0,0 +1,18 @@
+fetch ('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=8c90844a9e11a877c90ae40480aebcc2&units=metric')
+
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    condition.textContent = "Current Condition: "+data.main.temp+"\u00B0C";
+    wind.textContent = "Wind: "+ data.wind.speed + "..." + data.wind.deg;
+    icon.src = ("https://openweathermap.org/img/wn/") + data.weather[0].icon + "@2x.png";
+    
+});
+
+const toggleBTN = document.getElementById('toggle'); 
+const mainMenu = document.getElementById('navigation');
+
+toggleBTN.addEventListener('click', function() {
+    toggleBTN.classList.toggle('open');
+    mainMenu.classList.toggle('active');
+})
